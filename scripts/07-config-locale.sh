de55c580fe24247a1ccacca088f0b1e123bc2073
rou2terh4ck <157560886+rou2ter@users.noreply.github.com>
2026-06-07 22:21:14 +0700

    en_US.UTF-8

--- scripts/07-config-locale.sh
+++ scripts/07-config-locale.sh
@@ -53,16 +53,16 @@ cat > rootdir/etc/locale.gen << 'EOF'
 en_US.UTF-8 UTF-8
 zh_CN.UTF-8 UTF-8
 EOF
-chroot rootdir locale-gen zh_CN.UTF-8
-chroot rootdir update-locale LANG=zh_CN.UTF-8 LANGUAGE=zh_CN:zh
+chroot rootdir locale-gen en_US.UTF-8
+chroot rootdir update-locale LANG=en_US.UTF-8 LANGUAGE=en_US:en
 
 # 配置动态语言切换（SSH使用中文，TTY使用英文）
 cat > rootdir/etc/profile.d/99-locale-fix.sh << 'EOF'
 # 如果是SSH连接，则使用中文
 if [ -n "$SSH_CONNECTION" ] || [ -n "$SSH_TTY" ]; then
-    export LANG=zh_CN.UTF-8
-    export LANGUAGE=zh_CN:zh
-    export LC_ALL=zh_CN.UTF-8
+    export LANG=en_US.UTF-8
+    export LANGUAGE=en_US:en
+    export LC_ALL=en_US.UTF-8
 fi
 EOF
 chmod +x rootdir/etc/profile.d/99-locale-fix.sh
